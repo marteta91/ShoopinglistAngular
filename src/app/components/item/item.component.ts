a2119f97669da30cf793305366fb4fcfb20a918f
marteta91 <martaguitart938@gmail.com>
2022-03-07 22:37:34 +0100

    changes

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+
+import { Item } from 'src/app/models/item';
+import { EventEmitter } from 
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+
+  @Input() item: Item = new Item();
+  @Output() deleteItem: EventEmitter<Item> = new EventEmitter();
+  @Output() toggleItem: EventEmitter<Item> = new EventEmitter();
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(item: Item){
+    this.deleteItem.emit(item);
+  }
+  onToggle(item: Item){
+    item.completed = !item.completed;
+    this.toggleItem.emit(item);
+  }
+
+}
